43a266ee20eb529e1798fc4c8fe66a01d589ae98
Luke M <44519853+lkdm@users.noreply.github.com>
2025-02-22 17:50:21 +1100

    wip

--- 1shot.sh
+++ 1shot.sh
@@ -3,33 +3,60 @@
 set -e  # Exit on any error
 trap 'echo "An error occurred. Exiting..."; exit 1' ERR
 
-IMAGE="${ONESHOT_IMAGE:-alpine:latest}"
+IMAGE="alpine:latest"  # Set default image
+
 
 show_help() {
-    echo "Usage: 1shot [-i] [-s 'script'] [command]"
-    echo "  -i          : Start interactive mode"
-    echo "  -s 'script' : Execute the provided script"
-    echo "  help        : Show this help message"
-    echo "If no arguments are provided, this help message is shown."
+    echo "Usage: 1shot <command> [options]
+
+Commands:
+  run <command>        Run a single command in a container
+  shell                Start an interactive shell in a container
+  help                 Show this help message
+
+Options:
+  -i                   Specify a target image, default=alpine:latest
+
+Podman must be installed.
+  "
 }
 
-podman machine init 2>&1 | grep -v "VM already exists" || true
-podman machine start 2>&1 | grep -v "already running" || true
+init() {
+	if [[ "$1" == "-i" || "$1" == "--image" ]]; then
+    	if [[ -n "$2" ]]; then
+        	IMAGE="$2"
+        	shift 2
+    	else
+        	echo "No image specified after -i/--image flag. Using default: alpine:latest"
+        	shift 1
+    	fi
+	fi
+
+	if [[ "$1" == "-i" || "$1" == "--image" ]]; then
+    	IMAGE="$2"
+    	shift 2
+	fi
+
+	podman machine init 2>&1 | grep -v "VM already exists" || true
+	podman machine start 2>&1 | grep -v "already running" || true
+}
 
 case "$1" in
-    -i)
+    shell)
+    	init
         podman run -it --rm \
             -v "$HOME:$HOME" \
             -w "$HOME" \
             -e "PS1=\[\033[1;32m\]1shot\[\033[0m\]:\[\033[1;34m\]\w\[\033[0m\]^ " \
             "$IMAGE" /bin/sh
         ;;
-    -s)
+    run)
         if [ -z "$2" ]; then
             echo "Error: No script provided with -s option"
             show_help
             exit 1
         fi
+        init
         podman run -i --rm \
             -v "$HOME:$HOME" \
             -w "$HOME" \
