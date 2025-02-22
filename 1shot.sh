b14c13605e9d1b6e1a558af0ea46918d7738d181
Luke M <44519853+lkdm@users.noreply.github.com>
2025-02-22 17:06:01 +1100

    feat: allow changing image.

--- 1shot.sh
+++ 1shot.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+IMAGE="${ONESHOT_IMAGE:-alpine:latest}"
+
 podman machine init 2>&1 | grep -v "VM already exists" || true
 podman machine start 2>&1 | grep -v "already running" || true
 
@@ -8,13 +10,13 @@ if [ $# -eq 0 ]; then
         -v "$HOME:$HOME" \
         -w "$HOME" \
         -e "PS1=\[\033[1;32m\]1shot\[\033[0m\]:\[\033[1;34m\]\w\[\033[0m\]^ " \
-        alpine:latest /bin/sh
+        $IMAGE /bin/sh
 else
     # Arguments provided, treat as bash script
     podman run -i --rm \
         -v "$HOME:$HOME" \
         -w "$HOME" \
-        alpine:latest /bin/sh -c "$*"
+        $IMAGE /bin/sh -c "$*"
 fi
 
 set -e  # Exit on any error
